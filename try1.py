6155df3e24e95500f12c65575b1e748d131e6418
Stv <dan.steve2910@gmail.com>
2019-03-27 21:30:38 +0700

    zsc

--- try1.py
+++ try1.py
@@ -1,13 +1,28 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.stats import zscore
 
 file = r'training-data.xlsx'
 df = pd.read_excel(file)
+
+df.drop(['Appliances'], axis = 1, inplace = True)
+df.drop(['lights'], axis = 1, inplace = True)
 
 print(df.head(5))
 
 print(df.dtypes)
 
-df.boxplot()
+df = df.replace('?',np.NaN)
+df = df.replace('',np.NaN)
+df = df.replace(' ',np.NaN)
+df = df.replace('-',np.NaN)
+df = df.replace('_',np.NaN)
+
+#print(df.isnull().sum())
+for col in df.columns:
+    print('\t%s: %d' % (col,df[col].isnull().sum()))
+
+Z = df.apply(zscore)
+Z.boxplot()
 plt.show()
